5be63933e7fff059c40116fa88e2e591a9f6c9b9
Stanislav Lysenko <ministr.ua@ua.fm>
2020-05-13 19:31:26 +0300

    attempt

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/todolist'));
+app.use(express.static(__dirname + '/dist/todolist-by-stas'));
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/dist/todolist/index.html');
+  res.sendFile(__dirname + '/dist/todolist-by-stas/index.html');
 });
 
 app.listen(process.env.PORT || 8080);
